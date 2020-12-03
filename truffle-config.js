4f578be4442685562957633b6e6f5e609cc0aed8
Remote Gildor <73232606+remote-gildor@users.noreply.github.com>
2020-12-03 23:42:45 +0100

    compiler version added to config

--- truffle-config.js
+++ truffle-config.js
@@ -4,4 +4,10 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "vapp/src/contracts"),
 
+  compilers: {
+    solc: {
+      version: "0.6.8"    // Fetch exact version from solc-bin (default: truffle's version)
+    },
+  },
+
 };
